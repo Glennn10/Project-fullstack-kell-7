a1ba6b5c16418a7e4da60b09fc26a6aafa842019
Nobel Abel <alzafrannobel5@gmail.com>
2026-04-29 21:25:02 +0700

    update bookmode.js

--- backend/src/models/bookModel.js
+++ backend/src/models/bookModel.js
@@ -13,28 +13,32 @@ const BookModel = {
         return result.rows[0];
     },
 
+    // Create: Tambah buku baru (Update tambah cover_image)
     createBook: async (bookData) => {
-        const { title, author, publisher, year, category_id } = bookData;
+        const { title, author, publisher, year, category_id, cover_image } = bookData;
         const query = `
-            INSERT INTO books (title, author, publisher, year, category_id)
-            VALUES ($1, $2, $3, $4, $5)
+            INSERT INTO books (title, author, publisher, year, category_id, cover_image)
+            VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING *;
         `;
-
-        const values = [title, author, publisher, year, category_id];
+        const values = [title, author, publisher, year, category_id, cover_image];
         const result = await pool.query(query, values);
         return result.rows[0];
     },
 
+    // Update: Ubah data buku (Update tambah cover_image)
     updateBook: async (id, bookData) => {
-        const { title, author, publisher, year, category_id } = bookData;
+        const { title, author, publisher, year, category_id, cover_image } = bookData;
+        
+        // Kita pakai trik COALESCE di PostgreSQL. 
+        // Artinya: Kalau cover_image yang dikirim null (gak upload foto baru), pake nama foto yang lama aja.
         const query = `
             UPDATE books 
-            SET title = $1, author = $2, publisher = $3, year = $4, category_id = $5
-            WHERE id = $6
+            SET title = $1, author = $2, publisher = $3, year = $4, category_id = $5, cover_image = COALESCE($6, cover_image)
+            WHERE id = $7
             RETURNING *;
         `;
-        const values = [title, author, publisher, year, category_id, id];
+        const values = [title, author, publisher, year, category_id, cover_image, id];
         const result = await pool.query(query, values);
         return result.rows[0];
     },
